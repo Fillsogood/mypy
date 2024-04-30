c9d2d50c7d949d78176ac5df0aa78088e04b7c61
Fillsogood <jdd05273@naver.com>
2024-04-30 20:51:39 +0900

    register ci

--- app/settings.py
+++ app/settings.py
@@ -7,10 +7,8 @@ from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -33,7 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "article",
+
 ]
 
 MIDDLEWARE = [
@@ -64,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "testdjango.wsgi.application"
+WSGI_APPLICATION = "app.wsgi.application"
 
 
 # Database
